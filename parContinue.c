e5f48753270a622cad31995e02f5bf8957c1af25
Maria Eduarda R. Gabriel <142342951+MariaEduardaRibeiro0212@users.noreply.github.com>
2023-10-20 10:42:46 -0300

    Create parContinue.c
    Printar números pares entre 1 e 10 com o comando continue.

---
+++ parContinue.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	int i= 0;
+	
+	for(i; i <= 10; i++){
+		if(i % 2 != 0){
+			continue;
+		}
+		printf("%d\n", i);
+	}
+}
